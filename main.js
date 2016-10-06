a065a09ef670fdd6cb0ce407db9b5208b5f83148
mal46 <mal46@alu.ua.es>
2016-10-06 19:41:46 +0200

    Añadida lista de errores

--- main.js
+++ main.js
@@ -5,12 +5,16 @@
 angular.module('MyLBT', ['ngRoute']).config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
+    	when('/', {
+        templateUrl: 'listErrors.html',
+        controller: 'MainController'
+      }).
       when('/newError', {
         templateUrl: 'newError.html',
         controller: 'MainController'
       }).
       otherwise({
-        redirectTo: '/newError'
+        redirectTo: '/'
       });
   }]);
 
@@ -20,6 +24,38 @@ angular.module('MyLBT', ['ngRoute']).config(['$routeProvider',
 angular.module('MyLBT').controller('MainController', ['$scope','$window','$http', '$rootScope', function ($scope, $window, $http, $rootScope) {    
     
 	$scope.saludo="Hola";   
+	$scope.errors=[
+			{
+					id: 1,
+					gravity: 'serious',
+					type: 'minor',
+					creator: 'gonzalez',
+					devsInvolved: ['dev1', 'dev2', 'dev4'],
+					shortDescription: 'dskjaslkdj dklsajdlka slkdjalksdj dlksajdlk lksdjalksj slkdjaklj',
+					status: 'pending',
+					closed: false
+			},
+			{
+					id: 2,
+					gravity: 'serious',
+					type: 'minor',
+					creator: 'gonzalez',
+					devsInvolved: ['dev1', 'dev2', 'dev3'],
+					shortDescription: 'dskjaslkdj dklsajdlka slkdjalksdj dlksajdlk lksdjalksj slkdjaklj',
+					status: 'resolved',
+					closed: false
+			},
+			{
+					id: 3,
+					gravity: 'serious',
+					type: 'minor',
+					creator: 'gonzalez',
+					devsInvolved: ['dev1', 'dev4'],
+					shortDescription: 'dskjaslkdj dklsajdlka slkdjalksdj dlksajdlk lksdjalksj slkdjaklj',
+					status: 'not an error',
+					closed: true
+			}
+	]
 
 }]);
 

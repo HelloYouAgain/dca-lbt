d0b7381b48f7af8baca5be2452f4942c3ee2f43a
mal46 <mal46@alu.ua.es>
2016-10-11 08:52:12 +0200

    Finalizada

--- main.js
+++ main.js
@@ -7,11 +7,19 @@ angular.module('MyLBT', ['ngRoute']).config(['$routeProvider',
     $routeProvider.
     	when('/', {
         templateUrl: 'listErrors.html',
-        controller: 'MainController'
+        controller: 'listErrorsController'
       }).
       when('/newError', {
         templateUrl: 'newError.html',
-        controller: 'MainController'
+        controller: 'newErrorController'
+      }).
+			when('/error/:id', {
+        templateUrl: 'errorDetails.html',
+        controller: 'errorDetailsController'
+      }).
+			when('/error/:id/edit', {
+        templateUrl: 'editError.html',
+        controller: 'editErrorController'
       }).
       otherwise({
         redirectTo: '/'
@@ -19,43 +27,114 @@ angular.module('MyLBT', ['ngRoute']).config(['$routeProvider',
   }]);
 
 
+
+
+
+
+
 //  --- Main Controller ---
 
-angular.module('MyLBT').controller('MainController', ['$scope','$window','$http', '$rootScope', function ($scope, $window, $http, $rootScope) {    
+angular.module('MyLBT').controller('listErrorsController', ['$scope','$window','$http', '$rootScope', function ($scope, $window, $http, $rootScope) {      
+	$scope.errors=[];  
     
-	$scope.saludo="Hola";   
-	$scope.errors=[
-			{
-					id: 1,
-					gravity: 'serious',
-					type: 'minor',
-					creator: 'gonzalez',
-					devsInvolved: ['dev1', 'dev2', 'dev4'],
-					shortDescription: 'dskjaslkdj dklsajdlka slkdjalksdj dlksajdlk lksdjalksj slkdjaklj',
-					status: 'pending',
-					closed: false
-			},
-			{
-					id: 2,
-					gravity: 'serious',
-					type: 'minor',
-					creator: 'gonzalez',
-					devsInvolved: ['dev1', 'dev2', 'dev3'],
-					shortDescription: 'dskjaslkdj dklsajdlka slkdjalksdj dlksajdlk lksdjalksj slkdjaklj',
-					status: 'resolved',
-					closed: false
-			},
-			{
-					id: 3,
-					gravity: 'serious',
-					type: 'minor',
-					creator: 'gonzalez',
-					devsInvolved: ['dev1', 'dev4'],
-					shortDescription: 'dskjaslkdj dklsajdlka slkdjalksdj dlksajdlk lksdjalksj slkdjaklj',
-					status: 'not an error',
-					closed: true
-			}
-	]
+	$http.get('/getErrorsArray').success(function(res){
+			$scope.errors=res.data;
+	}).error(function(){
+			console.log("error");
+	});
 
 }]);
+
+
+
+
+angular.module('MyLBT').controller('newErrorController', ['$scope','$window','$http', '$rootScope', function ($scope, $window, $http, $rootScope) {    
+	$scope.error = {
+		id: 0,
+		gravity: '',
+		type: '',
+		creator: {
+			email: '',
+			name:''
+		},
+		devsInvolved: {dev1: false, dev2: false, dev3: false, dev4: false},
+		shortDescription: '',
+		largeDescription: '',
+		status: 'pendiente',
+			comments:[]
+	}
+	
+	$scope.saveError = function(){
+		//Enviar nuevo error a node y alli almacenarlo (y ponerle el id que le corresponda)
+		var obj = {data: $scope.error};
+    $http.post('/postNewError',obj).success(function(data) {
+				$window.location.href="#/";
+    }).error(function(data) {
+        console.log("error in posting");
+    });
+	}
+	
+}]);
+
+
+
+
+
+angular.module('MyLBT').controller('errorDetailsController', ['$scope','$window','$http', '$rootScope', '$routeParams', function ($scope, $window, $http, $rootScope, $routeParams) {   //Obtener error 
+	$scope.error;
+	
+	$http.get('/getError/'+$routeParams.id).success(function(res){
+			$scope.error=res.data;
+	}).error(function(){
+			console.log("error");
+	});
+	
+	$scope.newComment = {
+		author: {
+			name: '',
+			email: ''
+		},
+		message: ''
+	}
+	
+	$scope.saveComment = function(){
+		var nnewComment = {
+			author: {
+				name: $scope.newComment.author.name,
+				email: $scope.newComment.author.email
+			},
+			message: $scope.newComment.message
+		}
+		$scope.error.comments.push(nnewComment);
+		var obj = {data: $scope.error};
+    $http.post('/postChangesError/'+$routeParams.id,obj).success(function(data) {
+    }).error(function(data) {
+        console.log("error in posting");
+    });
+	}
+}]);
+
+
+
+
+
+angular.module('MyLBT').controller('editErrorController', ['$scope','$window','$http', '$rootScope', '$routeParams', function ($scope, $window, $http, $rootScope, $routeParams) {   
+	$scope.error;
+	$http.get('/getError/'+$routeParams.id).success(function(res){
+			$scope.error=res.data;
+	}).error(function(){
+			console.log("error");
+	});
+	
+	
+		$scope.saveErrorChanges = function(){
+		var obj = {data: $scope.error};
+    $http.post('/postChangesError/'+$routeParams.id,obj).success(function(data) {
+				$window.location.href="#/error/"+$routeParams.id;
+    }).error(function(data) {
+        console.log("error in posting");
+    });
+	}
+}]);
+
 
--- server.js
+++ server.js
@@ -1,90 +1,156 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/'))
 
-/*
-//Para la utilidad body-parser
-var bp = require('body-parser')
-app.use(bp.json())
 
-
-var lista = new Map()
-lista.set(1, {id:1, nombre:"patatas", cantidad:"1 bolsa"})
-lista.set(2, {id:2, nombre:"whisky",cantidad:"2 botellas"})
-var idActual = 3;
-
-
-app.get('/api/items', function(pet, resp){
-	var array = [];
-	lista.forEach(function(valor){
-		array.push(valor)
-	})
-	resp.send(array)
-})
-
-app.get('/api/items/:id', function(pet,resp){
-	var id = parseInt(pet.params.id)
-	if (isNaN(id)) {
-		resp.status(400);
-		resp.end();
+//Variables de almacenaje de datos
+var errorsArray=[
+			{
+			id: 1,
+			gravity: 'serio',
+			type: 'menor',
+			creator: {
+				email: 'Jose.p@gm.com',
+				name:'Jose'
+			},
+			devsInvolved: {dev1: true, dev2: false, dev3: true, dev4: false},
+			shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim cursus ultrices. Etiam efficitur feugiat lectus id consectetur. Curabitur in ligula sapien. Fusce elementum scelerisque accumsan.',
+			largeDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim cursus ultrices. Etiam efficitur feugiat lectus id consectetur. Curabitur in ligula sapien. Fusce elementum scelerisque accumsan. Donec facilisis eros sed porttitor tempus. Sed gravida non velit condimentum luctus. Vivamus a sodales sapien. Praesent ultricies varius magna, id interdum purus mollis id. Cras dapibus purus nec dictum laoreet. Quisque venenatis orci sit amet lectus pharetra congue. Ut tortor mi, congue tristique sem et, vulputate vestibulum lacus. Phasellus at condimentum enim. Suspendisse sagittis mattis risus ac interdum. Ut ut sodales ligula, tempor facilisis urna.',
+			status: 'listado',
+			comments:[
+				{
+					author: {
+						name: 'Carlos',
+						email: 'c.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				},
+				{
+					author: {
+						name: 'Pedro',
+						email: 'p.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				},
+				{
+					author: {
+						name: 'Jose',
+						email: 'j.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				}
+			]
+	},
+			{
+			id: 2,
+			gravity: '',
+			type: '',
+			creator: {
+				email: 'Pepe.llb@gm.com',
+				name:'Pepe'
+			},
+			devsInvolved: {dev1: false, dev2: false, dev3: false, dev4: false},
+			shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim cursus ultrices. Etiam efficitur feugiat lectus id consectetur. Curabitur in ligula sapien. Fusce elementum scelerisque accumsan.',
+			largeDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim cursus ultrices. Etiam efficitur feugiat lectus id consectetur. Curabitur in ligula sapien. Fusce elementum scelerisque accumsan. Donec facilisis eros sed porttitor tempus. Sed gravida non velit condimentum luctus. Vivamus a sodales sapien. Praesent ultricies varius magna, id interdum purus mollis id. Cras dapibus purus nec dictum laoreet. Quisque venenatis orci sit amet lectus pharetra congue. Ut tortor mi, congue tristique sem et, vulputate vestibulum lacus. Phasellus at condimentum enim. Suspendisse sagittis mattis risus ac interdum. Ut ut sodales ligula, tempor facilisis urna.',
+			status: 'pendiente',
+			comments:[
+				{
+					author: {
+						name: 'Carlos',
+						email: 'c.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				},
+				{
+					author: {
+						name: 'Pedro',
+						email: 'p.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				},
+				{
+					author: {
+						name: 'Jose',
+						email: 'j.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				}
+			]
+	},
+			{
+			id: 3,
+			gravity: 'grave',
+			type: 'importante',
+			creator: {
+				email: 'Manuel.js@gm.com',
+				name:'Manuel'
+			},
+			devsInvolved: {dev1: true, dev2: false, dev3: false, dev4: false},
+			shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim cursus ultrices. Etiam efficitur feugiat lectus id consectetur. Curabitur in ligula sapien. Fusce elementum scelerisque accumsan.',
+			largeDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dignissim cursus ultrices. Etiam efficitur feugiat lectus id consectetur. Curabitur in ligula sapien. Fusce elementum scelerisque accumsan. Donec facilisis eros sed porttitor tempus. Sed gravida non velit condimentum luctus. Vivamus a sodales sapien. Praesent ultricies varius magna, id interdum purus mollis id. Cras dapibus purus nec dictum laoreet. Quisque venenatis orci sit amet lectus pharetra congue. Ut tortor mi, congue tristique sem et, vulputate vestibulum lacus. Phasellus at condimentum enim. Suspendisse sagittis mattis risus ac interdum. Ut ut sodales ligula, tempor facilisis urna.',
+			status: 'resuelto',
+			comments:[
+				{
+					author: {
+						name: 'Carlos',
+						email: 'c.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				},
+				{
+					author: {
+						name: 'Pedro',
+						email: 'p.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				},
+				{
+					author: {
+						name: 'Jose',
+						email: 'j.vill@gm.com'
+					},
+					message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+				}
+			]
 	}
-	else {
-		var item = lista.get(id)
-		if (item==undefined) {
-			resp.status(404)
-			resp.send('No existe el item con id ' + id);
-		}
-		else
-			resp.send(item);
-	}
-})
+	]
 
-//para probar con curl
-//curl -d '{"nombre":"tomates","cantidad":1}' -H "Content-Type:application/json" -v http://localhost:3000/api/items
-app.post('/api/items', function(pet, resp) {
-   var nuevo = pet.body
-   if (nuevo.nombre && nuevo.cantidad) {
-     var creado = {id: idActual, nombre:nuevo.nombre, cantidad: nuevo.cantidad}
-	 lista.set(idActual,creado)
-	 idActual++
-	 resp.status(201)
-	 //Fundamentalismo REST
-	 resp.header('Location','http://localhost:3000/api/items/'+creado.id) 
-	 //En la práctica muchos APIs devuelven el objeto creado, incluyendo id
-	 resp.send(creado)    
-   }
-   else {
-   	 resp.status(400)
-   	 resp.send("el objeto no tiene los campos adecuados")
-   }
-})
+//Consultas
+app.get('/getErrorsArray', function (req, res) {
+   res.json({data: errorsArray}); 
+});
 
-//Podéis probar esto con
-//curl -X DELETE -v http://localhost:3000/api/items/1
-app.delete('/api/items/:id', function(pet, resp){
-	var id = parseInt(pet.params.id)
-	if (isNaN(id)) {
-		resp.status(400);
-		resp.end();
-	}
-	else {
-		var item = lista.get(id)
-		if (item==undefined) {
-			resp.status(404)
-			resp.send('No existe el item con id ' + id);
-		}
-		else{
-			lista.delete(id);
-			resp.end();
-		}
-	}
-})*/
+app.get('/getError/:id', function (req, res) {
+   res.json({data: errorsArray[req.params.id-1]}); 
+});
+
+app.post('/postNewError', function(req, res) {
+		var error = req.body.data;
+		error.id = errorsArray.length+1;
+		errorsArray.push(error);
+    res.end();
+});
+
+app.post('/postChangesError/:id', function(req, res) {
+		var error = req.body.data;
+		errorsArray[req.params.id-1] = error;
+    res.end();
+});
 
 app.get('/', function(req, res) {   // Carga una vista HTML simple donde irá nuestra Single App Page. Angular Manejará el Frontend
     res.sendFile(__dirname+'/index.html');             
 });
 
 app.listen(3000,function(){
-	console.log('Marchando el servidor...')
+	console.log('Marchando el servidor en el puerto 3000...')
 
 })
+
+
